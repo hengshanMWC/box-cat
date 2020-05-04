f30e0fb71dc42dea59af0dc9cdd650dc985cf3f4
mwc <1819293933@qq.com>
2020-05-04 16:47:38 +0800

    chore: 重构完成

--- utils/regExp.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function getFormat(str: string, key?: string): string[];
-export declare function getESC(str: string): string;
-export declare function getRegFinish(str: string): string;
-export declare function createSliceRegExp(str1: string, str2: string): RegExp;
-export declare function createParamsRegExp(str1: string, str2: string): RegExp;
