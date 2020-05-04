5c97497121c23db891b49492f7edeb09d803da2f
mwc <1819293933@qq.com>
2020-05-04 16:50:47 +0800

    chore: git上传问题

---
+++ build/remove.config.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+// 删除ts多余文件
+function deleteFolder(path) {
+  let files = [];
+  if( fs.existsSync(path) ) {
+      files = fs.readdirSync(path);
+      files.forEach(function(file,index){
+          let curPath = path + "/" + file;
+          if(fs.statSync(curPath).isDirectory()) {
+              deleteFolder(curPath);
+          } else {
+              fs.unlinkSync(curPath);
+          }
+      });
+      fs.rmdirSync(path);
+      console.log(`删除${path}文件夹成功`)
+  }
+}
+deleteFolder(path.join(__dirname, '../dist/utils'))
